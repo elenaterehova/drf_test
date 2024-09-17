b15cd6e4749a2abc93cfbe0efaf9de2b84fc67ab
elenaterehova <136344744+elenaterehova@users.noreply.github.com>
2024-09-17 15:41:21 +0300

    Update urls.py

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,11 +4,6 @@ from rest_framework import routers
 from .views import *
 
 
-  # 'Authorization': 'Token 2001f864612daa65b58627cd09dd1ff5e2ca0a5fd6'
-    # username = elenaterehova
-    # password = elenaterehova
-
-
 router = routers.DefaultRouter()
 router.register(r'countries', CountryViewSet)
 router.register(r'manufacturers', ManufacturerViewSet)
